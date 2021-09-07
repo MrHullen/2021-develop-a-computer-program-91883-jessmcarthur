56e17574d981848a7608f5b29744c5508a224c3a
Jess <mcarthje16@craighead.school.nz>
2021-09-07 13:34:22 +1200

    Updated
    I have written my code better

--- guess-the-number.js
+++ guess-the-number.js
@@ -1,16 +1,17 @@
 // Your code goes in this file.
+function guessNumber(){
+const randomNum = Math.floor(Math.random() * 20) + 1
+
 alert ('You are playing guess the number. You have to pick a number between 1 and 20 (inclusive). You have 3 guesses to get it right!')
 let newName = prompt ('What is your name?')
 
-const random = Math.floor(Math.random() * 20) + 1
+let guess = prompt ('Hi ' + newName + ' Enter a number between 1 and 20')
 
-let newNum = prompt ('Hi ' + newName + ' Guess a number from 1 to 20')
-
-while(newNum !== random) {
-    let newNum = prompt ('Guess a number from 1 to 20')
-}
-
-if(number == random) {
-    alert('Correct! Would you like to play again?')
-    ''
+    if(guess < randomNum) {
+        alert('Your guess is too low')
+    } else if(guess > randomNum) {
+        alert('Your guess is too high')
+    } else if(guess==randomNum) {
+        alert('You guessed correct!')
+    }
 }
