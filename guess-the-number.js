1fef76711b9606ff166a87f8c9618b35abd53071
Jess <mcarthje16@craighead.school.nz>
2021-09-20 12:17:56 +1200

    Get users name
    Expected Value Tests:
    "Jess" - passed
    "Sara" - passed
    "Lauraine" - passed

--- guess-the-number.js
+++ guess-the-number.js
@@ -1,23 +1,29 @@
 // Your code goes in this file.
 function guessNumber() {
+    //creates a random number
     let randomNum = Math.floor(Math.random() * 20) + 1
     alert(randomNum)
-
+    //variables
     let maxTries = 3
     let counter = 0
 
     alert('You are playing guess the number. You have to pick a number between 1 and 20 (inclusive). You have 3 guesses to get it right!')
-    let name = prompt('What is your name?')
-
+    //asks for the users name
+    getName()
+    
+    //starts the first while loop
     while (counter != maxTries) {
         counter += 1
         let guess = prompt('Guess a number between 1 and 20')
-
         if (guess != randomNum) {
-            alert('Incorrect!')
+            alert('Incorrect! ' + name)
         } else {
-            (guess == randomNum)
-            alert('Correct!')
+            counter = maxTries
+            alert('Correct! ' + name)
         }
     }
+}
+
+function getName() {
+    let name = prompt('What is your name?')
 }
