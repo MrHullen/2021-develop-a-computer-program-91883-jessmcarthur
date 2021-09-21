f690899437fd6e7e8c33b687a8edeb7a3f51b900
Jess <mcarthje16@craighead.school.nz>
2021-09-21 14:09:44 +1200

    Finished code
    Finished code!!!

--- guess-the-number.js
+++ guess-the-number.js
@@ -1,21 +1,23 @@
 // Your code goes in this file.
+//creates function to start the game
 function play() {
     getName()
     playGame()
 }
-
+//creates function to get the users name
 function getName() {
     let name = prompt('What is your name?')
     alert('Hi ' + name + '. You are playing guess the number. You need to guess a number between 1 and 20 (inclusive). You have 3 tries to get it right.')
 }
-
+//creates a function to play the game
 function playGame() {
     let playAgain = 'yes'
-
+    //creates a while loop to play the game again
     while (playAgain == 'yes') {
+        //creates a random number
         const randomNum = Math.floor(Math.random() * 20) + 1
-        alert(randomNum)
 
+        //creates 2 variables
         let maxTries = 3
         let counter = 0
 
@@ -30,7 +32,7 @@ function playGame() {
                 alert('Correct!')
             }
         }
-        //plays the game again
+        //asks the user if they want to play again
         playAgain = prompt('Would you like to play again?')
     }
 }
