dba1e0b39447a072323f76579f0512beed66f453
Jess <mcarthje16@craighead.school.nz>
2021-09-21 13:56:50 +1200

    play again testing
    testing if it lets the user play the game again when entering 'yes'
    
    expected testing: plays again
    passed!

--- guess-the-number.js
+++ guess-the-number.js
@@ -1,29 +1,36 @@
 // Your code goes in this file.
-function guessNumber() {
-    //creates a random number
-    let randomNum = Math.floor(Math.random() * 20) + 1
-    alert(randomNum)
-    //variables
-    let maxTries = 3
-    let counter = 0
-
-    alert('You are playing guess the number. You have to pick a number between 1 and 20 (inclusive). You have 3 guesses to get it right!')
-    //asks for the users name
+function play() {
     getName()
-    
-    //starts the first while loop
-    while (counter != maxTries) {
-        counter += 1
-        let guess = prompt('Guess a number between 1 and 20')
-        if (guess != randomNum) {
-            alert('Incorrect! ' + name)
-        } else {
-            counter = maxTries
-            alert('Correct! ' + name)
-        }
-    }
+    playGame()
 }
 
 function getName() {
     let name = prompt('What is your name?')
+    alert('Hi ' + name + ' You are playing guess the number. You need to guess a number between 1 and 20 (inclusive). You have 3 tries to get it right.')
+}
+
+function playGame() {
+    let playAgain = 'yes'
+
+    while (playAgain == 'yes') {
+        const randomNum = Math.floor(Math.random() * 20) + 1
+        alert(randomNum)
+
+        let maxTries = 3
+        let counter = 0
+
+        //while loop  plays the game
+        while (counter != maxTries) {
+            counter += 1
+            let guess = prompt('Guess a number between 1 and 20')
+            if (guess != randomNum) {
+                alert('Incorrect!')
+            } else {
+                counter = maxTries
+                alert('Correct!')
+            }
+        }
+        //plays the game again
+        playAgain = prompt('Would you like to play again?')
+    }
 }
